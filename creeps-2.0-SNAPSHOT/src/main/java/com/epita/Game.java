c88eddf8c56236f5dc84ea266eff065903c91347
Matthieu Archambault <matthieu.archambault@epita.fr>
2019-04-20 12:33:42 +0200

    feat(Game): implem of the gameLoop function and her utils functions

--- creeps-2.0-SNAPSHOT/src/main/java/com/epita/Game.java
+++ creeps-2.0-SNAPSHOT/src/main/java/com/epita/Game.java
@@ -1,6 +1,9 @@
 package com.epita;
 
+import com.epita.creeps.given.extra.Cartographer;
 import com.epita.creeps.given.vo.response.InitResponse;
+import com.epita.creeps.given.vo.response.StatisticsResponse;
+import com.epita.creeps.given.vo.response.StatusResponse;
 import com.epita.tools.GenericRequest;
 import com.epita.units.Nexus;
 import com.epita.units.Probe;
@@ -11,8 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by: Matthieu Archambault
@@ -26,10 +27,72 @@ public class Game
     String login;
     String commitHash;
     ScheduledThreadPoolExecutor tpe;
+    int tickrate;
+    int ticks;
+    int minerals;
+    int biomass;
+    int warmUpTime;
+
     List <Unit> unitList;
 
+    public void initGame() throws UnirestException {
+        InitResponse b = GenericRequest.genericPost(url + "/init/" + this.login + "-" + this.commitHash
+                , InitResponse.class);
+        this.tickrate = (int) b.setup.ticksPerSeconds;
+        System.out.println(this.tickrate);
 
+        StatisticsResponse statisticsResponse = getStatistics();
+        this.warmUpTime = statisticsResponse.scheduledGameStartTick;
+        Cartographer.initialize(statisticsResponse.dimension);
 
+        unitList.add(new Nexus(this, b.coordinates, b.baseId));
+        unitList.add(new Probe(this, b.coordinates, b.probeId));
+    }
+
+    public boolean isRunning() throws UnirestException {
+        StatusResponse res = GenericRequest.genericGet(this.url + "/status", StatusResponse.class);
+        if (res != null)
+            return res.running;
+        return false;
+    }
+
+    public StatisticsResponse getStatistics() throws UnirestException {
+        StatisticsResponse res =  GenericRequest.genericGet(url + "/statistics", StatisticsResponse.class);
+        return res;
+    }
+
+    public StatisticsResponse.PlayerStatsResponse getPlayerResponse() throws UnirestException {
+        StatisticsResponse statisticsResponse = getStatistics();
+        for (StatisticsResponse.PlayerStatsResponse e : statisticsResponse.players)
+        {
+            String name = this.login + "-" + commitHash;
+            if (name.equals(e.name))
+            {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public void gameLoop() throws InterruptedException, ExecutionException, UnirestException {
+        while (isRunning())
+        {
+            StatisticsResponse.PlayerStatsResponse playerStatsResponse = getPlayerResponse();
+            this.minerals = playerStatsResponse.minerals;
+            this.biomass = playerStatsResponse.biomass;
+
+            for (int i = 0; i < unitList.size(); i++) {
+                Unit u = unitList.get(i);
+                if (u instanceof Nexus) {
+                    ((Nexus) u).initProbe();
+                }
+                if (u instanceof Probe) {
+                    ((Probe) u).moveUnitWest();
+                }
+            }
+        }
+
+    }
     public Game(String url, String login, String commitHash) throws UnirestException, ExecutionException, InterruptedException {
         this.url = url;
         this.login = login;
@@ -37,28 +100,16 @@ public class Game
         this.tpe = new ScheduledThreadPoolExecutor(8);
         this.unitList = new ArrayList<>();
 
+        this.initGame();
 
-
-        InitResponse b = GenericRequest.genericPost(url + "/init/" + this.login + "-" + this.commitHash
-                , InitResponse.class);
-
-        unitList.add(new Nexus(this, b.coordinates, b.baseId));
-        unitList.add(new Probe(this, b.coordinates, b.probeId));
-
-        for (Unit u : unitList)
-        {
-            if (u instanceof Nexus)
-            {
-                System.out.println(((Nexus) u).initProbe().toString());
-            }
-            if (u instanceof Probe)
-            {
-                System.out.println(((Probe) u).moveUnitWest().toString());
-            }
+        if(!isRunning()) {
+            Thread.sleep(this.warmUpTime * 1000 / this.tickrate);
         }
 
+        gameLoop();
 
     }
+
 
     public List<Unit> getUnitList() {
         return unitList;
@@ -78,5 +129,17 @@ public class Game
 
     public ScheduledThreadPoolExecutor getTpe() {
         return tpe;
+    }
+
+    public int getTickrate() {
+        return tickrate;
+    }
+
+    public int getMinerals() {
+        return minerals;
+    }
+
+    public int getBiomass() {
+        return biomass;
     }
 }
