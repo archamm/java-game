028a10808301fdc056ffb44064ba9e4ed88718c4
Matthieu Archambault <matthieu.archambault@epita.fr>
2019-04-20 12:28:29 +0200

    fix(Observer): implem of Scans two and four directly in observer

--- creeps-2.0-SNAPSHOT/src/main/java/com/epita/units/Observer.java
+++ creeps-2.0-SNAPSHOT/src/main/java/com/epita/units/Observer.java
@@ -2,7 +2,16 @@ package com.epita.units;
 
 import com.epita.Game;
 
+import com.epita.callables.CallableInspectReport;
+import com.epita.callables.CallableSpawnReport;
+import com.epita.creeps.given.extra.Cartographer;
+import com.epita.creeps.given.vo.Block;
 import com.epita.creeps.given.vo.geometry.Point;
+import com.epita.creeps.given.vo.report.SpawnReport;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by: Matthieu Archambault
@@ -16,5 +25,15 @@ public class Observer extends MovingUnits
         this.coordinates = coordinates;
         this.agentId = agentId;
         this.name = "observer";
+    }
+
+
+    public void ScanTwo() throws ExecutionException, InterruptedException {
+        Cartographer.INSTANCE.register(sendCommandGetScanReport("/scan:2", 1));
+    }
+
+
+    public void ScanFour() throws ExecutionException, InterruptedException {
+        Cartographer.INSTANCE.register(sendCommandGetScanReport("/scan:4", 1));
     }
 }
