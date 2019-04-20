7c5d1c17adb9f63568d0e1130b22cc4e2de93f15
Matthieu Archambault <matthieu.archambault@epita.fr>
2019-04-20 12:23:28 +0200

    feat(Dragoon): implem Dragoon

---
+++ creeps-2.0-SNAPSHOT/src/main/java/com/epita/units/Dragoon.java
@@ -0,0 +1,22 @@
+package com.epita.units;
+
+import com.epita.Game;
+import com.epita.creeps.given.vo.geometry.Point;
+
+/**
+ * Created by: Matthieu Archambault
+ * On 2019-04-20 09:24
+ */
+public class Dragoon extends Unit
+{
+    public Dragoon(Game game, Point coordinates, String agentId)
+    {
+        this.game = game;
+        this.coordinates = coordinates;
+        this.agentId = agentId;
+        this.name = "dragoon";
+    }
+
+
+
+}
