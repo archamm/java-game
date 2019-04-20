9c79db8a51d79e41d8ae744cc70543988be2f994
Matthieu Archambault <matthieu.archambault@epita.fr>
2019-04-20 12:30:50 +0200

    fix(Unit): implem of generic functions directly in Units

--- creeps-2.0-SNAPSHOT/src/main/java/com/epita/units/Unit.java
+++ creeps-2.0-SNAPSHOT/src/main/java/com/epita/units/Unit.java
@@ -1,13 +1,11 @@
 package com.epita.units;
 
 import com.epita.Game;
-import com.epita.creeps.given.vo.report.MoveReport;
-import com.epita.creeps.given.vo.report.Report;
+import com.epita.callables.CallableCommand;
+import com.epita.callables.CallableSpawnReport;
+import com.epita.creeps.given.vo.report.SpawnReport;
 import com.epita.creeps.given.vo.response.CommandResponse;
-import com.epita.tools.CallableReport;
-import com.epita.tools.GenericRequest;
-import com.epita.tools.CallableCommand;
-import com.mashape.unirest.http.exceptions.UnirestException;
+
 
 import com.epita.creeps.given.vo.geometry.Point;
 ;
@@ -17,18 +15,38 @@ import java.util.concurrent.*;
  * Created by: Matthieu Archambault
  * On 2019-04-19 20:30
  */
+
+
 public class Unit {
     Game game;
     Point coordinates;
     String name;
     String agentId;
 
+    public SpawnReport sendCommandGetSpawnReport(String cmd, int waitTime) throws ExecutionException, InterruptedException {
+
+        ScheduledFuture<SpawnReport> report = this.game.getTpe().schedule(new CallableSpawnReport(this, this.sendCommand(cmd).reportId)
+                , 1000 * waitTime / this.game.getTickrate(), TimeUnit.MILLISECONDS);
+        return report.get();
+    }
 
     public CommandResponse sendCommand(String cmd) throws ExecutionException, InterruptedException {
         Callable<CommandResponse> callableRes = null;
             Future<CommandResponse> res = this.game.getTpe().submit(new CallableCommand(this, cmd));
             return res.get();
         }
+
+    public void SearchAndReplaceCoordinates(Point point)
+    {
+        for (Unit unit : this.game.getUnitList())
+        {
+            if(unit.agentId.equals(this.agentId)) {
+                unit.setCoordinates(point);
+            }
+        }
+    }
+
+
 
 
 
@@ -46,6 +64,20 @@ public class Unit {
 
     public String getAgentId() {
         return agentId;
+    }
+
+    public void setCoordinates(Point coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Unit{" +
+                "game=" + game +
+                ", coordinates=" + coordinates +
+                ", name='" + name + '\'' +
+                ", agentId='" + agentId + '\'' +
+                '}';
     }
 }
 
